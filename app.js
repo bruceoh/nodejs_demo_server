5cf9405c34c1e3f849b8361ad48df26479ba6a73
Bruce Oh <brucegwanhooh@ucla.edu>
2019-05-14 17:15:40 +0900

    Adding some extentions and dependencies

--- app.js
+++ app.js
@@ -1,19 +1,15 @@
-var http  = require('http');
-var express = require('express');
+var http = require("http");
+var express = require("express");
 var app = express();
 var port = 3001;
-var v1 = require('./routes/v1');
-
+var v1 = require("./routes/v1");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use('/', v1);
+app.use("/v1", v1);
 
+app.get("/", function(req, res) {
+  res.send("Hello World");
+});
 
-app.get('/', function (req, res) {
-   res.send('Hello World');
-})
-
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
-
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
--- routes/v1.js
+++ routes/v1.js
@@ -1,5 +1,3 @@
-
-
 var router = require('express').Router();
 
 router.get('/api', (req, res) => {
