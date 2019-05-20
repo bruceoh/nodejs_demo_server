1ffcffb5506871bc0d429906a1eb404d1a873091
Bruce Oh <bruceo@beflex.co>
2019-05-20 14:28:40 +0900

    adding more routes

--- routes/api/v1.js
+++ routes/api/v1.js
@@ -6,17 +6,56 @@ router.get('/test', (req, res) => {
 
 router.get('/', (req, res, next) => {
     res.status(200).json({
-        message: 'Handling GET request to v1'
+        message: 'Handling GET requests to /products'
     });
 });
 
 router.post('/', (req, res, next) => {
-    res.status(200).json({
-        message: 'Handling POST request to v1'
+    res.status(201).json({
+        message: 'Handling POST requests to /products'
     });
 });
 
+router.get('/:productId', (req, res, next) => {
+    const id = req.params.productId;
+    if (id === 'special') {
+        res.status(200).json({
+            message: 'You discovered the special ID',
+            id: id
+        });
+    } else {
+        res.status(200).json({
+            message: 'You passed an ID'
+        });
+    }
+});
 
+router.patch('/:productId', (req, res, next) => {
+    res.status(200).json({
+        message: 'Updated product!'
+    });
+});
+
+router.delete('/:productId', (req, res, next) => {
+    res.status(200).json({
+        message: 'Deleted product!'
+    });
+});
+
+router.post('/users', function(req, res, next){
+    var user = new User();
+  
+    user.username = req.body.user.username;
+    user.email = req.body.user.email;
+    user.setPassword(req.body.user.password);
+  
+    user.save().then(function(){
+      return res.json({user: user.toAuthJSON()});
+    }).catch(next);
+  });
+
+  
+  
 
 module.exports = router;
 
